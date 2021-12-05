4bc836a41c567d56cbed06c62d538842390d160a
Polina Sinkevich <linasinkevich111@gmail.com>
2021-12-05 04:56:34 +0300

    create a simple variant of User exchange

---
+++ usermgmt_client/usermgmt_client.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	pb "CourseProject/usermgmt/usermgmt"
+	"context"
+	"google.golang.org/grpc"
+	"log"
+	"time"
+)
+
+const (
+	address = "localhost:50051"
+)
+
+func main() {
+	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+	if err!=nil{
+		log.Fatalf("did not connect %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewUserManagementClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	var newUsers = make(map[string]int32)
+	newUsers["Andrew"] = 34
+	newUsers["Ann"] = 33
+	for name, age := range newUsers {
+		r, err := c.CreateNewUser(ctx, &pb.NewUser{Name: name, Age: age})
+		if err!=nil{
+			log.Fatalf("could not create User: %v", err)
+		}
+		log.Printf("User Details:" +
+			" Name: %s" +
+			" Age: %d " +
+			"Id: %d",
+			r.GetName(), r.GetAge(), r.GetId())
+	}
+
+}
